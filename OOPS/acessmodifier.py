497278f6fada66438cd228f4816323409c2b8ee5
Souvik Mandal <souvikmandals10@gmail.com>
2025-06-09 02:08:09 +0530

    new file

---
+++ OOPS/acessmodifier.py
@@ -0,0 +1,15 @@
+# private public acess in OOPS
+class Employee:
+    def __init__(self):
+        self.__name="Harry"
+    def _funname(self):
+        return "Codewithme"
+
+class Project(Employee):
+    pass
+
+e = Project()
+# print(e.__name) cant be acessed like that
+print(e._Employee__name)  # can be acessed inderctly  NAME MANGLING
+print(e._funname()) # private acess through child
+
