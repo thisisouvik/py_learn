78fd6e4a6ae72c72ce103800f69eefa6002e4386
Souvik Mandal <souvikmandals10@gmail.com>
2025-06-09 01:17:54 +0530

    some changes

--- OOPS/getter&setter.py
+++ OOPS/getter&setter.py
@@ -5,9 +5,9 @@ class MyClass:
     def show(self):
         print(f"Return my {self._value} value")
     
-    @property
+    @property 
     def ten_value(self):
-        return self._value
+        return 10* self._value
     
     @ten_value.setter
     def ten_value(self,new_value):
@@ -15,8 +15,10 @@ class MyClass:
     
     
 obj = MyClass(10)
-obj.ten_value= 67
+obj.ten_value= 67 
 print(obj.ten_value)
 obj.show()
-
+ 
+ 
+ # Take Input -> do /10 division -> return 6.7-> do *10 and return 67.0
     
