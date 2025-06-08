bc8b72f7637b86581f10cedbb5ab34fb52fd43ad
Souvik Mandal <souvikmandals10@gmail.com>
2025-06-08 23:29:20 +0530

    Separate files for each sections

---
+++ OOPS/decorator.py
@@ -0,0 +1,11 @@
+#Decorator
+def greet(fx):
+    def mfx():
+        print("Good Morning")
+        fx()
+        print("Thanks")
+        return mfx
+    @ greet
+    def hello():
+        print("HEllo World")
+    hello()
