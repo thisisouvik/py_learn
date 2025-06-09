8005c86805100385c052c53a3ccf053bf6ea0572
Souvik Mandal <souvikmandals10@gmail.com>
2025-06-10 01:50:56 +0530

    edited

--- OS Module/clearclutter.py
+++ OS Module/clearclutter.py
@@ -1,7 +1,8 @@
 import os
 
-# os.rename("py_learn/OS Module/CS logo.png", "py_learn/OS Module/1.png") for renaming file name
+# os.rename("py_learn/OS Module/CS logo.png", "py_learn/OS Module/1.png")     (for renaming file name)
 
+# If the existing files are named like 1.png, 2.png, etc., this may cause overwriting or a FileExistsError
 
 folder = "py_learn/OS Module"
 files = [f for f in os.listdir(folder) if f.endswith(".png")]
@@ -11,3 +12,4 @@ for i, file in enumerate(files, 1):
     dst = os.path.join(folder, f"{i}.png")
     print(f"Renaming {file} to {i}.png")
     os.rename(src, dst)
+
