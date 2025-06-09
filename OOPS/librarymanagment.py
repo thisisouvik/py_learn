1a1c2dd6782d46996a9353163db7bf5f36323cc3
Souvik Mandal <souvikmandals10@gmail.com>
2025-06-10 01:03:56 +0530

    ubdated file

--- OOPS/librarymanagment.py
+++ OOPS/librarymanagment.py
@@ -1,16 +1,17 @@
-class Library1:
-    def __init__(self,books,no_of_books):
-        self.bname=books
-        self.noofbooks=no_of_books
+class Library:
+    def __init__(self):
+        self.books=[]
+        self.nobooks=0
 
-class Library2:
-    def __init__(self,books,no_of_books):
-        self.bname=books
-        self.noofbooks=no_of_books
-        
-class Library3:
-    def __init__(self,books,no_of_books):
-        self.bname=books
-        self.noofbooks=no_of_books
+    def addbook(self,book):
+        self.books.append(book)
+        self.nobooks=len(self.books)
 
+    def show(self):
+        print(f"Name of book {self.books} and number of book {self.nobooks}")
 
+l1=Library()
+l1.addbook("Harry Potter")
+l1.addbook("King")
+l1.addbook("Harry 2")
+l1.show()
