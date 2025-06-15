a65a24e5cd4e32009426072cdd82aa1232b244fd
Souvik Mandal <souvikmandals10@gmail.com>
2025-06-15 23:44:08 +0530

    new folder

--- PDF merge/pypdf.py
+++ PDF merge/pypdf.py
@@ -1,1 +1,12 @@
-import pypdf as pdf
+from pypdf import PdfWriter
+import os
+
+folder = "."  # Set this to your PDF files directory
+merger=PdfWriter()
+files=[f for f in os.listdir(folder) if f.endswith(".pdf")]
+
+for pdf in files:
+    merger.append(pdf)
+
+merger.write("Mergedpdf.pdf")
+merger.close()
