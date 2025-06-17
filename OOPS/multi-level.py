88b3789f187924a8b19e27b82b3d33f315448f8c
Souvik Mandal <souvikmandals10@gmail.com>
2025-06-17 16:35:23 +0530

    added new file

---
+++ OOPS/multi-level.py
@@ -0,0 +1,27 @@
+#Hybrid Inheritance
+
+class Baseclass:
+    pass
+
+class derived1:
+    pass
+
+class derived2:
+    pass
+class derived3(derived1,derived2):
+    pass
+
+#Hiercical Inheritance
+
+class son:
+    pass
+
+class father(son):
+    pass
+
+class grandfather(father):
+    pass
+
+class ggfather(grandfather):
+    pass
+
