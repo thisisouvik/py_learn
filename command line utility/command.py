5b34e890b58b497ab6d6aff95be7c2e85f23c80f
Souvik Mandal <souvikmandals10@gmail.com>
2025-06-18 15:16:47 +0530

    new file

---
+++ command line utility/command.py
@@ -0,0 +1,14 @@
+import argparse
+import requests
+
+def download_file(url, local_filename):
+    local_filename=url.split('/')[-1]
+    with requests.get(url, stream=True) as r:
+        r.raise_for_status()
+        with open(local_filename, 'wb+')as f:
+            for chunk in r.iter_content(chunk_size=8192):
+                f.write(chunk)
+    return(local_filename)
+
+parser=argparse.ArgumentParser()
+parser.add_argument
