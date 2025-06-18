10e33e1679850bb8c16445621f7624c4657c28e0
Souvik Mandal <souvikmandals10@gmail.com>
2025-06-18 17:50:51 +0530

    new file

---
+++ shutil module/main.py
@@ -0,0 +1,7 @@
+import shutil 
+
+shutil.copy("copy1.py", "copy2.py")  #file
+
+shutil.copytree(".file", "file2") #folder
+
+shutil.move("py_learn/shutil module/copy1.py","py_learn/copy1.py" ) #moving file 
