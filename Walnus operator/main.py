8bb413e8dc2f4e19182d4a10fecdb68c14edd00a
Souvik Mandal <souvikmandals10@gmail.com>
2025-06-18 16:49:36 +0530

    edit file

---
+++ Walnus operator/main.py
@@ -0,0 +1,14 @@
+#Aam Zindagi
+
+foods =list()
+while True:
+    food=input("Enter food : ")
+    if food == "quit":
+        break
+    foods.append(food)
+
+#walnus zindagi
+
+cookies=list()
+while (cookie := input("What cookie you like : ")) != "quit":
+    cookies.append(cookie)
