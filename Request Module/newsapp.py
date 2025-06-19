4d7f1c2c908979d4b1dccdd75299651908f06df6
Souvik Mandal <souvikmandals10@gmail.com>
2025-06-19 16:31:17 +0530

    feat: new file

---
+++ Request Module/newsapp.py
@@ -0,0 +1,15 @@
+import requests
+import json
+
+news=input("Enter topic of news : ")
+
+url=(f"https://newsapi.org/v2/everything?q={news}&from=2025-05-19&sortBy=publishedAt&apiKey={apikey}")
+
+r = requests.get(url)
+news=json.loads(r.text)
+
+for article in news['articles']:
+    print('Author :' , article['author'])
+    print('Title : ',article['title'])
+    print('Descrption :' ,article['description'])
+    print("--------------------------")
