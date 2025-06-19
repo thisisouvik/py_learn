762d9b46a7b8e62cf8feab48bf1d330677d325ce
Souvik Mandal <souvikmandals10@gmail.com>
2025-06-19 17:54:07 +0530

    feat: new file

---
+++ function caching/main.py
@@ -0,0 +1,22 @@
+from functools import lru_cache
+import time
+
+@lru_cache(maxsize=None)       #memoisation
+def fx(n):
+    time.sleep(5)
+    return n*5
+
+print(fx(20))
+print("done for 20")
+print(fx(2))
+print("done for 2")
+print(fx(4))
+print("done for 4")
+
+print(fx(12))
+print("Done for 12")
+print(fx(2))
+print("done for 2")
+print(fx(4))
+print("done for 4")
+
--- generator.py
+++ generator.py
@@ -2,7 +2,7 @@
 
 def my_generator():
     for i in range(50):
-        yield i
+        yield i          #generator use kiya ja raha hai
 
 gen=my_generator()
 
