a01474ac8f59d2e906d7312f2de8b763fdcd2de8
Souvik Mandal <souvikmandals10@gmail.com>
2025-08-16 20:46:04 +0530

    learn : first code

--- FastAPI/app.py
+++ FastAPI/app.py
@@ -1,1 +1,12 @@
 from fastapi import FastAPI
+
+api = FastAPI()
+
+#GET - getting information from  server
+#POST- creating/submit in the server
+#PUT - putting req in server
+#DELETE - deleting something in server
+
+@api.get('/')
+def index():
+    return
