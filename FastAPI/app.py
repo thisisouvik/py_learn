d839c955abc6243de99028e75910ca6f5e713cba
Souvik Mandal <souvikmandals10@gmail.com>
2025-08-20 18:42:28 +0530

    learn: get endpnt

--- FastAPI/app.py
+++ FastAPI/app.py
@@ -7,6 +7,29 @@ api = FastAPI()
 #PUT - putting req in server
 #DELETE - deleting something in server
 
+all_todos = [
+    {'todo_id': 1, "role": "Student", "Desc": "Jobless"},
+    {'todo_id': 2, "role": "Freak", "Desc": "Pitambara"},
+    {'todo_id': 3, "role": "Manager", "Desc": "Managing"},
+    {'todo_id': 4, "role": "Guard", "Desc": "Guarding"},
+    {'todo_id': 5, "role": "Plumber", "Desc": "Plumbing"}
+]
+
 @api.get('/')
 def index():
-    return
+    return {"message" : "Hello World"}
+
+
+@api.get('/todos/{todo_id}')
+def get_todo(todo_id: int):
+    for todo in all_todos:
+        if todo['todo_id'] == todo_id:
+            return {'result': todo}
+        
+@api.get('/todos')
+def get_all_todo(first_n: int = None):
+    if first_n:
+        return all_todos[:first_n]
+    else:
+        return all_todos
+    
