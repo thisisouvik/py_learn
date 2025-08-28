4f829203c7b4fe9d2e7333986fc7e8653002e73e
Souvik Mandal <souvikmandals10@gmail.com>
2025-08-28 23:09:25 +0530

    learn: pydantic schemas

--- FastAPI/app.py
+++ FastAPI/app.py
@@ -16,8 +16,16 @@ class TodoBase(BaseModel):
     todo_description:str =Field(..., description='Description of the todo')
     priority: Priority =Field(default=Priority.LOW, description='Priority of the todo')
     
-    
+class TodoCreate(TodoBase):
+    pass
 
+class Todo(TodoBase):
+    todo_id: int = Field(..., description='Unique identifier of the todo')
+
+class TodoUbdate(BaseModel):
+    todo_names: Optional[str] = Field(..., min_length=3, max_length=512, description= 'Name of the todo')
+    todo_description: Optional[str] =Field(..., description='Description of the todo')
+    priority: Optional[Priority] =Field(default=Priority.LOW, description='Priority of the todo')
 
 #GET - getting information from  server
 #POST- creating/submit in the server
@@ -25,11 +33,12 @@ class TodoBase(BaseModel):
 #DELETE - deleting something in server
 
 all_todos = [
-    {'todo_id': 1, "role": "Student", "Desc": "Jobless"},
-    {'todo_id': 2, "role": "Freak", "Desc": "Pitambara"},
-    {'todo_id': 3, "role": "Manager", "Desc": "Managing"},
-    {'todo_id': 4, "role": "Guard", "Desc": "Guarding"},
-    {'todo_id': 5, "role": "Plumber", "Desc": "Plumbing"}
+    Todo(todo_id=1, todo_name= "Clae_huse", todo_description= "Cleaing houses the hutha", priority=Priority.HIGH),
+    Todo(todo_id=2, todo_name= "Clae_huse1", todo_description= "1Cleaing houses the hutha", priority=Priority.MEDIUM),
+    Todo(todo_id=3, todo_name= "Clae_huse2", todo_description= "2Cleaing houses the hutha", priority=Priority.MEDIUM),
+    Todo(todo_id=4, todo_name= "Clae_huse3", todo_description= "3Cleaing houses the hutha", priority=Priority.LOW),
+    Todo(todo_id=5, todo_name= "Clae_huse4", todo_description= "4Cleaing houses the hutha", priority=Priority.LOW)
+    
 ]
 
 @api.get('/')
@@ -37,21 +46,21 @@ def index():
     return {"message" : "Hello World"}
 
 
-@api.get('/todos/{todo_id}')
+@api.get('/todos/{todo_id}', response_model= Todo)
 def get_todo(todo_id: int):
     for todo in all_todos:
         if todo['todo_id'] == todo_id:
             return {'result': todo}
         
-@api.get('/todos')
+@api.get('/todos', response_model=List[Todo])
 def get_all_todo(first_n: int = None):
     if first_n:
         return all_todos[:first_n]
     else:
         return all_todos
     
-@api.post('/todos')
-def create_todo(todo : dict):
+@api.post('/todos', response_model=Todo)
+def create_todo(todo : TodoCreate):
     new_todo_id= max(todo['todo_id'] for todo in all_todos) + 1
 
     new_todo = {
