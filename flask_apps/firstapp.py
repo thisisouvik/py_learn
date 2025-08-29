bec72dcdceb3e03943f462d1661bbbe98b80598d
Souvik Mandal <souvikmandals10@gmail.com>
2025-08-30 01:20:29 +0530

    routes and url

--- flask_apps/firstapp.py
+++ flask_apps/firstapp.py
@@ -6,5 +6,13 @@ app = Flask(__name__)
 def index():
     return "<h1>HEllo world</h1>"
 
+@app.route('/xyz')
+def hello():
+    return "Hello World"
+
+@app.route('/greet/<name>')
+def greet(name):
+    return f"Hello {name}"
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug = True)
+    app.run(host='0.0.0.0', port = 5555 ,debug = True)
