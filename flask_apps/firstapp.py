a4da4a491d032a2b95ac94aa107dffefe9440f10
Souvik Mandal <souvikmandals10@gmail.com>
2025-08-31 11:35:41 +0530

    handle param req

--- flask_apps/firstapp.py
+++ flask_apps/firstapp.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask , request
 
 app = Flask(__name__)
 
@@ -13,6 +13,19 @@ def hello():
 @app.route('/greet/<name>')
 def greet(name):
     return f"Hello {name}"
+
+@app.route('/add/<int:num1>/<int:num2>')
+def add(num1, num2):
+    return f' {num1} + {num2} = {num1+num2}'
+
+@app.route('/handle_url_params')
+def handle_params():
+    if 'greeting' in request.args.keys() and 'name' is request.args.keys():
+        greeting = request.args['greeting']
+        name = request.args['name']
+        return f'{greeting} {name}'
+    else:
+        return "Some parameters missing"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port = 5555 ,debug = True)
