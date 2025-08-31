b7cb1f1a0a864d256b413c6353a2333488e836d2
Souvik Mandal <souvikmandals10@gmail.com>
2025-08-31 16:48:55 +0530

    adjusted base index and form model

--- flask_apps/firstapp.py
+++ flask_apps/firstapp.py
@@ -2,10 +2,20 @@ from flask import Flask , request, make_response, render_template
 
 app = Flask(__name__, template_folder= 'templates')
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    mylist = [10, 12, 13]
-    return render_template(template_name_or_list='index.html', my_list=mylist)
+    if request.method == 'GET':
+        return render_template('index.html')
+    
+    elif request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+
+        if username =='souvik' and password== 'password':
+            return f'Welcome {username}'
+        else:
+            return 'Invalid credentials'
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port = 5555 ,debug = True)
