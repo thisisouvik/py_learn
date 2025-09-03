e3cac943f108bbc077e35326170d5b3531545410
Souvik Mandal <souvikmandals10@gmail.com>
2025-09-03 18:18:37 +0530

    set and get data

--- flask_apps/firstapp.py
+++ flask_apps/firstapp.py
@@ -1,10 +1,26 @@
-from flask import Flask , request, make_response, render_template, jsonify
+from flask import Flask , request, make_response, render_template, jsonify, session
 
-app = Flask(__name__, template_folder= 'templates', static_folder= 'static', static_url_path='/')
+app = Flask(__name__, template_folder= 'templates')
+
+app.secret_key = 'SOME_KEY'
 
 @app.route('/')
 def index():
     return 'Hello World'
+
+@app.route('/set_data')
+def set_data():
+    session['name']= 'Mike'
+    session['other'] = 'Hello World'
+    return render_template(template_name_or_list= 'index.html', message = ' Session data set')
+
+@app.route('get_data')
+def get_data():
+    Name = session['name']
+    others = session['other']
+    return render_template(template_name_or_list='index.html', message= f'Name : {Name}, Other: {others}')
+
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port = 5555 ,debug = True)
