25388facc10486d3880dc923ecd43a92b62bf570
Souvik Mandal <souvikmandals10@gmail.com>
2025-09-03 18:45:24 +0530

    connected sessiond dataa

--- flask_apps/firstapp.py
+++ flask_apps/firstapp.py
@@ -6,21 +6,32 @@ app.secret_key = 'SOME_KEY'
 
 @app.route('/')
 def index():
-    return 'Hello World'
+    # Render the index template so the form, buttons and session links are visible
+    return render_template('index.html')
 
 @app.route('/set_data')
 def set_data():
-    session['name']= 'Mike'
+    session['name'] = 'Mike'
     session['other'] = 'Hello World'
-    return render_template(template_name_or_list= 'index.html', message = ' Session data set')
+    # pass session values and a message to the template
+    return render_template('index.html', message='Session data set', name=session.get('name'), other=session.get('other'))
 
-@app.route('get_data')
+@app.route('/get_data')
 def get_data():
-    Name = session['name']
-    others = session['other']
-    return render_template(template_name_or_list='index.html', message= f'Name : {Name}, Other: {others}')
+    name = session.get('name')
+    other = session.get('other')
 
+    if not name and not other:
+        message = 'No session data found. Call Set Session Data first.'
+    else:
+        message = f'Name: {name}, Other: {other}'
+    return render_template('index.html', message=message, name=name, other=other)
 
+@app.route('clear_data')
+def clear_data():
+    session.clear()
+
+    return render_template('index.html', )
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port = 5555 ,debug = True)
