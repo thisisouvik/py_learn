a51c071627b8a880511cc33ee0e7665cbfb8b08a
Souvik Mandal <souvikmandals10@gmail.com>
2025-09-05 19:54:19 +0530

    initialise db

---
+++ dbapplication/app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+db = SQLAlchemy()
+
+def create_app():
+    app = Flask(__name__, template_folder='templates')
+    app.config('SQLALCHEMY_DATABASE_URI') = 'sqlite:///./testdb.db'
+
+    db.init_app(app)
+
+    
