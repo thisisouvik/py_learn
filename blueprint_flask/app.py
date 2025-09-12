74f604e7c7842b3b65b073ad429a10b0cba7bf08
Souvik Mandal <souvikmandals10@gmail.com>
2025-09-12 20:48:26 +0530

    merged all routes files in app backend

---
+++ blueprint_flask/app.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+db=SQLAlchemy()
+
+def create_app():
+    app = Flask(__name__, template_folder='templates')
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///./blueprints.db'
+
+    db.init_app(app)
+
+    from blueprintapp.blueprints.todos.routes import todos
+
+    app.register_blueprint(todos, url_prefix='/todos')
+
+    migrate = Migrate(app, db)
+
+    return app
